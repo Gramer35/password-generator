dc02b80941f8fba2094649a76927c52f3b56ea4f
Chris Gramer <gramer3514@gmail.com>
2023-03-07 18:31:00 -0700

    added code for password

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var password = "";
+  // THEN I am presented with a series of prompts for password criteria
+
+  // THEN I select which criteria to include in the password
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  const passwordLength = prompt("How long would you like your password?");
+
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  var lowercase = confirm("Do you want lowercase?");
+  var uppercase = confirm("Do you want uppercase?");
+  var numeric = confirm("Do you want numeric?");
+  var special = confirm("Do you want special?");
+
+  // THEN my input should be validated and at least one character type should be selected
+
+  // THEN a password is generated that matches the selected criteria
+
+  // THEN the password is either displayed in an alert or written to the page
+  return password;
+}
